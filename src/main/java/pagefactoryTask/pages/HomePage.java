c3339e28303a97d2dabe34114f6950e6d9a2c560
Maciej Wojciechowski <maciej.wojciechowski@outlook.com>
2018-09-23 15:15:00 +0200

    added menu test

--- src/main/java/pagefactoryTask/pages/HomePage.java
+++ src/main/java/pagefactoryTask/pages/HomePage.java
@@ -3,6 +3,9 @@ package pagefactoryTask.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomePage {
 
     @FindBy(css = ".navbar #li_myaccount")
@@ -11,8 +14,21 @@ public class HomePage {
     @FindBy(partialLinkText = "Login")
     private WebElement buttonLogIn;
 
+    @FindBy(xpath = "//ul[@class='nav navbar-nav go-right']")
+    List<WebElement> topMenuBarElements;
+
     public void clickOnLoginLink(){
         buttonMyAccout.click();
         buttonLogIn.click();
     }
+
+    public ArrayList<String> getMenuItems(){
+       ArrayList<String> availableMenuItems = new ArrayList<>();
+        for (WebElement e : topMenuBarElements) {
+            String temp = e.getText();//to jest jeden string i ma juz wszystko. nie ma sensu robic for trzeba tylko podzielic
+            availableMenuItems.add(temp);
+        }
+        return availableMenuItems;
+    }
+
 }
--- src/test/java/pagefactoryTask/ui/LoginTest.java
+++ src/test/java/pagefactoryTask/ui/LoginTest.java
@@ -10,6 +10,10 @@ import pagefactoryTask.pages.HomePage;
 import pagefactoryTask.pages.LoginPage;
 import pagefactoryTask.pages.MyAccoutPage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LoginTest {
@@ -18,6 +22,9 @@ public class LoginTest {
     private static final String USER_EMAIL = "user@phptravels.com";
     private static final String USER_PASS = "demouser";
     private static final String EXPECTED_WELCOME_TEXT = "Hi, Johny Smith";
+    private static final ArrayList<String> MENU_LIST =
+            new ArrayList<>(Arrays.asList("HOME","HOTELS","FLIGHTS","TOURS","CARS","VISA","OFFERS","BLOG"));
+
     private WebDriver driver;
     private HomePage homePage;
     private LoginPage loginPage;
@@ -39,10 +46,14 @@ public class LoginTest {
 
     @Test
     public void testLoginUser(){
-        homePage.clickOnLoginLink();
-        loginPage.loginUsingCredencials(USER_EMAIL, USER_PASS);
-        assertThat(myAccoutPage.getWelcomeMessage())
-                .isEqualTo(EXPECTED_WELCOME_TEXT);
+//        homePage.clickOnLoginLink();
+//        loginPage.loginUsingCredencials(USER_EMAIL, USER_PASS);
+//        assertThat(myAccoutPage.getWelcomeMessage())
+//                .isEqualTo(EXPECTED_WELCOME_TEXT);
+
+        assertThat(homePage.getMenuItems())
+                .isEqualTo(MENU_LIST);
+
     }
 
     @After
